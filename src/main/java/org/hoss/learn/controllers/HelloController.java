7addac1f1ae0daf5ac94bf9d68c35fe7ea68dd33
hossman <carl.hostrander00@gmail.com>
2019-07-13 20:45:53 -0500

    checkin of project

---
+++ src/main/java/org/hoss/learn/controllers/HelloController.java
@@ -0,0 +1,13 @@
+package org.hoss.learn.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String getGreeting() {
+        return "Hello world!";
+    }
+}
